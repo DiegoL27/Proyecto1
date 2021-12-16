e349d384d0776967a33ab2cc55efe0becd6342a8
DiegoL27 <diego.lopez.1894@gmail.com>
2021-12-16 19:07:29 -0300

    se agregan variables y se muestran en html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nuevo1';
+  title = 'nuevo1'; 
+  var1 = "hola" 
+  var2 = 1
 }
